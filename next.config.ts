2aaeae67730ef77877f15e767e91ace11d8d2325
st4rkis <116030951+st4rkis@users.noreply.github.com>
2026-02-10 11:06:12 +0200

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+
+  // REQUIRED so multi-domain works in dev
+  allowedDevOrigins: [
+    "http://pir.local:3000",
+    "http://her.local:3000",
+    "http://jmk.local:3000",
+    "http://jtr.local:3000",
+    "http://pas.local:3000",
+    "http://jnx.local:3000",
+    "http://tin.local:3000",
+    "http://mlo.local:3000",
+    "http://rho.local:3000",
+    "http://kgs.local:3000",
+    "http://cfu.local:3000",
+    "http://zth.local:3000",
+    "http://arg.local:3000",
+    "http://lef.local:3000",
+  ],
 };
 
 export default nextConfig;
